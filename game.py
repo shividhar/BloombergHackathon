6471188b387074b6e1189c26b7aba9ca1abef7d4
Yash Mathur <yash97mathur@gmail.com>
2017-03-04 04:24:59 -0500

    sdnkjsdv

--- game.py
+++ game.py
@@ -2,23 +2,43 @@ import socket
 import sys
 import time
 import math
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST, PORT = "codebb.cloudapp.net", 17429
+sock.connect((HOST, PORT))
+
+sock.sendall("4geese gee4se \n")
+
+def config():
+    print(run("CONFIGURATIONS"))
+
 
 class Player:
     def __init__(self):
         status = run("STATUS")
         status = status.split(" ")
-        self.x = status[1]
-        self.y = status[2]
-        self.speedX = status[3]
-        self.speedY = status[4]
+        self.x = float(status[1])
+        self.y = float(status[2])
+        self.speedX = float(status[3])
+        self.speedY = float(status[4])
+        self.prevBombDropTime = time.time()
         self.minesOwned = []
+        self.statusMines = []
+        self.scanMines = []
+
+        mineIndex = len(status) - status[::-1].index("MINES") - 1
+
+        currentIndex = mineIndex+2
+        for i in range(0,int(status[mineIndex+1])):
+            self.statusMines.append([status[currentIndex+1], status[currentIndex+2]])
+            currentIndex += 3
     def update(self):
-        status = run("4geese", "gee4se")
-        self.x = status[1]
-        self.y = status[2]
-        self.speedX = status[3]
-        self.speedY = status[4]
+        status = run("STATUS")
+        status = status.split(" ")
+        self.x = float(status[1])
+        self.y = float(status[2])
+        self.speedX = float(status[3])
+        self.speedY = float(status[4])
     def accelerate(self, angle, magnitude):
         run("ACCELERATE " + str(angle) + " " + str(magnitude))
     def brake(self):
@@ -27,52 +47,47 @@ class Player:
         run("BOMB " + str(x) + " " + str(y) + " " + str(t))
     def addMine(self, owner, x, y):
         self.minesOwned.append([owner, float(x), float(y)])
-
-class Mines:
-    def __init__(self):
-        self.statusMines = []
-        self.scanMines = []
-
+    def updateStatusMines(self):
+        # status = ["A", "B", "MINES", "3", "0", "0", "0", "1", "1", "1", "2", "2", "2"]
         status = run("STATUS")
         status = status.split(" ")
-
         mineIndex = len(status) - status[::-1].index("MINES") - 1
 
         currentIndex = mineIndex+2
 
+        mineFoundStatus = False
         for i in range(0,int(status[mineIndex+1])):
-            self.statusMines.append([status[currentIndex+1], status[currentIndex+2]])
+            try:
+                mineFoundIndex = self.statusMines.index([status[currentIndex], float(status[currentIndex+1]),float(status[currentIndex+2])])
+                self.statusMines.append(statusMines[mineFoundIndex])
+                del self.statusMines[mineFoundIndex]
+            except ValueError:
+                self.statusMines.append([status[currentIndex], float(status[currentIndex+1]),float(status[currentIndex+2])])
+            mineFoundStatus = True
             currentIndex += 3
-    def updateStatusMines(self):
-    	# status = ["A", "B", "MINES", "3", "0", "0", "0", "1", "1", "1", "2", "2", "2"]
-        status = run("STATUS")
-        status = status.split(" ")
-    	mineIndex = len(status) - status[::-1].index("MINES") - 1
-
-        currentIndex = mineIndex+2
-        mineFound = False
-        for i in range(0,int(status[mineIndex+1])):
-            self.statusMines.append([float(status[currentIndex+1]),float(status[currentIndex+2])])
-            currentIndex += 3
-            mineFound = True
-        return mineFound
+        return mineFoundStatus
     def updateScanMines(self, x, y):
-	    scan = run("SCAN " + str(x) + " " + str(y))
-	    if scan != "ERROR Scanning too soon":
-	    	scan = scan.split(" ")
+        scan = run("SCAN " + str(x) + " " + str(y))
+        if scan != "ERROR Scanning too soon":
+            scan = scan.split(" ")
 
-	    	mineIndex = len(scan) - scan[::-1].index("MINES") - 1
+            mineIndex = len(scan) - scan[::-1].index("MINES") - 1
 
-	    	currentIndex = mineIndex+2
-            mineFound = False
-	    	for i in range(0,int(scan[mineIndex+1])):
-	            self.scanMines.append([scan[currentIndex], float(scan[currentIndex+1]),float(scan[currentIndex+2])])
-	            currentIndex += 3
-                mineFound = True
-            return mineFound
-	    else:
-	    	print("SCANNING TOO SOON")
+            currentIndex = mineIndex+2
 
+            mineFoundStatus = False
+            for i in range(0,int(scan[mineIndex+1])):
+                try:
+                    mineFoundIndex = self.scanMines.index([scan[currentIndex], float(status[currentIndex+1]),float(status[currentIndex+2])])
+                    self.scanMines.append([scan[currentIndex], float(scan[currentIndex+1]),float(scan[currentIndex+2])])
+                    del self.scanMines[mineFoundIndex]
+                except ValueError:
+                    self.scanMines.append([scan[currentIndex], float(scan[currentIndex+1]),float(scan[currentIndex+2])])
+                mineFoundStatus = True
+                currentIndex += 3
+            return mineFoundStatus
+        else:
+            print("SCANNING TOO SOON")
 class Bombs:
     def __init__(self):
         self.bombs = []
@@ -97,6 +112,8 @@ class Bombs:
         for i in range(0,int(status[bombIndex+1])):
             self.bombs.append([float(status[currentIndex]),float(status[currentIndex+1])])
             currentIndex += 2
+
+
 
 def run(commands):
 
@@ -126,11 +143,12 @@ def run(commands):
 def getMineOwner(mx, my, player1):
 
     for i in range(0, len(player1.mines)):
-        if mx == player1.mines[i][1] and my == player1.mines[i][2]
-            return player1.mines[i][0]
+        if (mx == player1.scanMines[i][1] and my == player1.scanMines[i][2]):
+            return player1.scanMines[i][0]
     return ""
+
 def takeMine(mx, my):
-    player1 = new Player()
+    player1 = Player()
     player1.brake()
     while True:
         angle = math.atan2(my - player1.y, mx - player2.x)
@@ -140,9 +158,6 @@ def takeMine(mx, my):
 
     return [-1, -1]
 
-HOST, PORT = "codebb.cloudapp.net", 17429
-sock.connect((HOST, PORT))
-sock.sendall("4geese gee4se \n")
 
 
 player1 = Player()
@@ -161,6 +176,11 @@ a = [-math.pi/2, math.pi, math.pi/2, 0]
 while True:
     player1 = Player()
     #print("blah", float(player1.x), float(x)+300)
+
+    if player1.updateStatusMines():
+        print("IM FUCKING CLOSE")
+        takeMine(player1.statusMines[-1][1], player1.statusMines[-1][2])
+
     if hzr and math.fabs(float(player1.x) - float(x) - 300*d) < 100:
         print("STOPXR")
         x = player1.x
